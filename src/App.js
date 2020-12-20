82d4b600795b2bfa6e83f5f1997d682efa8e6c65
goudete <goudetenrique@gmail.com>
2020-12-20 12:07:26 -0600

    changed index.html title

--- src/App.js
+++ src/App.js
@@ -36,24 +36,6 @@ function App() {
         </Switch>
       </Router>
     </div>
-    // <div className="App">
-    //   <Navbaroo />
-    //   <header className="App-header">
-    //     <h1>
-    //       Welcome friend.
-    //     </h1>
-    //     <h3>
-    //       Have a look around. If you have any questions, <br />
-    //       This is where you can find me: <br />
-
-    //     </h3>
-    //     <p>
-          
-    //       Twitter: @EnriqueGoudet <br />
-    //       Instagram: @quiquegdt
-    //     </p>
-    //   </header>
-    // </div>
   );
 }
 
